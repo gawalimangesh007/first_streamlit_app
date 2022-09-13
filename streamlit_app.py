8e9e243126a52c2481a97fb334f98c804652e605
Mangesh Gawali <gawalimangesh007@gmail.com>
2022-09-12 20:23:58 -0400

    Update streamlit_app.py
    Added additional info

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
-streamlit.title('My Parents New Healthy Diner
-                Breakfast Menu
+streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text("""
 Omega 3 & Blueberry Oatmea
 Kale. Spinach & Rocket Smoothie
 Hard-Boiled Free-Range Egg
-                ')
+""")
